98feaf94229286cf7c7806224c4a0aee79e179d5
Jefffrey <notjefff@gmail.com>
2015-05-19 13:02:39 +0200

    Uniformed `aid::bitboard` to the `Matrix` concept

--- include/aid/bitboard.hpp
+++ include/aid/bitboard.hpp
@@ -8,21 +8,23 @@ namespace aid {
     class bitboard {
     public:
 
-        using bitset_type   = std::bitset<Rows * Cols>;
-        using reference     = bitset_type::reference;
+        using array_type        = std::bitset<Rows * Cols>;
+        using value_type        = typename array_type::value_type;
+        using reference         = typename array_type::reference;
+        using const_reference   = typename array_type::const_reference;
+        using size_type         = typename array_type::size_type;
+        using iterator          = typename array_type::iterator;
+        using const_iterator    = typename array_type::const_iterator;
+
+        static constexpr std::size_t col_size       = Rows;
+        static constexpr std::size_t row_size       = Cols;
+        static constexpr std::size_t matrix_size    = Rows * Cols;
 
         bitboard() = default;
+        bitboard(bitboard&&) = default;
         bitboard(bitboard const&) = default;
-
-        bitboard(bitboard&&) 
-            noexcept(std::is_nothrow_move_constructible(bitset_type)::value)
-            = default;
-
+        bitboard& operator=(bitboard&&) = default;
         bitboard& operator=(bitboard const&) = default;
-
-        bitboard& operator=(bitboard&&)
-            noexcept(std::is_nothrow_move_assignable(bitset_type)::value)
-            = default;
 
         reference operator()(std::size_t row, std::size_t col) {
             return bitset[row * Cols + col];
@@ -30,6 +32,20 @@ namespace aid {
 
         constexpr bool operator()(std::size_t row, std::size_t col) const {
             return bitset[row * Cols + col];
+        }
+
+        typename std::enable_if<
+            is_vector<bitboard>::value,
+            reference 
+        >::type operator[](size_type i) {
+            return bitset[i];
+        }
+
+        typename std::enable_if<
+            is_vector<bitboard>::value,
+            const_reference 
+        >::type operator[](size_type i) const {
+            return bitset[i];
         }
 
         bitboard& operator&=(bitboard const& rhs) {
@@ -47,12 +63,18 @@ namespace aid {
             return (*this);
         }
 
-        bitboard& operator<<=(std::size_t s) {
+        typename std::enable_if<
+            is_row_vector<bitboard>::value,
+            bitboard&
+        >::type operator<<=(std::size_t s) {
             bitset <<= s;
             return (*this);
         }
 
-        bitboard& operator>>=(std::size_t s) {
+        typename std::enable_if<
+            is_row_vector<bitboard>::value,
+            bitboard&
+        >::type operator>>=(std::size_t s) {
             bitset >>= s;
             return (*this);
         }
@@ -73,10 +95,11 @@ namespace aid {
         bool none() const { return bitset.none(); }
         bool any() const { return bitset.any(); }
 
-        constexpr std::size_t rows() const { return Rows; }
-        constexpr std::size_t columns() const { return Cols; }
-        constexpr std::size_t size() const { return Rows * Cols; }
-        std::size_t count() const { return bitset.count(); }
+        constexpr size_type rows() const { return Rows; }
+        constexpr size_type columns() const { return Cols; }
+        constexpr size_type size() const { return Rows * Cols; }
+        size_type count() const { return bitset.count(); }
+        constexpr bool empty() const { return size() == 0; }
 
         void swap(bitboard& rhs) noexcept {
             bitset.swap(rhs.bitset);
@@ -84,9 +107,12 @@ namespace aid {
 
     private:
 
-        std::bitset<Rows * Cols> bitset;
+        array_type bitset;
 
     };
+
+    template<std::size_t Rows, std::size_t Cols>
+    struct is_matrix<aid::bitboard<Rows, Cols>> : std::true_type {};
 
     template<std::size_t Rows, std::size_t Cols>
     bitboard<Rows, Cols> operator<<(bitboard<Rows, Cols> lhs, std::size_t s) {
