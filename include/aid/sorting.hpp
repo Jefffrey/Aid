3f4bcc208a56a09b31ea7f1d47d27a0ec43268a7
Jefffrey <notjefff@gmail.com>
2015-05-26 19:05:11 +0200

    Added insertion sort

--- include/aid/sorting.hpp
+++ include/aid/sorting.hpp
@@ -17,4 +17,15 @@ namespace aid {
                     std::iter_swap(it2, std::next(it2));
     }
 
+    // worst case: n^2
+    // average case: n^2
+    // best case: n
+    // space complexity: 1
+    template<typename It>
+    void insertion_sort(It first, It last) {
+        for (auto it = std::next(first); it != last; ++it)
+            for (auto it2 = it; it2 != first && *std::prev(it2) > *it2; --it2)
+                std::iter_swap(std::prev(it2), it2);
+    }
+
 }
