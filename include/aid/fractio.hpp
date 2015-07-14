e646c9fd0cf7058a657c9089c57575ad6426b742
Jefffrey <notjefff@gmail.com>
2015-07-14 04:08:46 +0200

    Added multiplication and division for fractio class

--- include/aid/fractio.hpp
+++ include/aid/fractio.hpp
@@ -1,26 +1,120 @@
 #pragma once
 
+#include <cassert>
+#include <type_traits>
+#include <iostream>
+
 namespace aid {
 
-    template<typename TypeNum, typename TypeDenum = TypeNum>
-    struct fractio {
-        fractio(TypeNum n, TypeDenum d)
-            : numerator(n)
-            , denumerator(d)
+    template<typename Type>
+    class fractio {
+    private:
+
+        Type num;
+        Type den;
+
+    public:
+
+        // intentionally not `explicit` to allow
+        // implicit conversion from integral types to
+        // fractio objects
+        template<
+            typename OtherType,
+            typename  = typename std::enable_if<
+                std::is_integral<Type>::value
+                    && std::is_integral<OtherType>::value
+            >::type
+        >
+        fractio(OtherType const& n)
+            : num(n)
+            , den(1)
             {}
 
-        fractio(TypeNum n)
-            : numerator(n)
-            , denumerator(1)
-            {}
+        template<
+            typename OtherType,
+            typename = typename std::enable_if<
+                std::is_integral<Type>::value
+                    && std::is_integral<OtherType>::value
+            >::type
+        >
+        fractio(OtherType const& n, OtherType const& d)
+            : num(n)
+            , den(d)
+            {
+            assert(den != 0);
+        }
 
-        TypeNum numerator;
-        TypeDenum denumerator;
+        Type numerator() const { return num; }
+        Type denominator() const { return den; }
+
+        fractio& invert() {
+            assert(num != 0);
+            std::swap(num, den);
+            return (*this);
+        }
+
+        template<typename TypeRhs>
+        fractio& operator*=(fractio<TypeRhs> const& rhs) {
+            num *= rhs.num;
+            den *= rhs.den;
+            return (*this);
+        }
+
+        template<typename TypeRhs>
+        fractio& operator/=(fractio<TypeRhs> rhs) {
+            return (*this) *= rhs.invert();
+        }
+
+        // the following functions are friend in order to instantiate
+        // them with the class and take advantage of the implicit conversion
+        // from integral types to fractio
+
+        friend fractio<Type> operator*(fractio<Type> lhs, fractio<Type> const& rhs) {
+            return lhs *= rhs;
+        }
+
+        friend fractio<Type> operator/(fractio<Type> lhs, fractio<Type> const& rhs) {
+            return lhs /= rhs;
+        }
+
     };
 
-    template<typename EvalType, typename TypeNum, typename TypeDenum>
-    EvalType evaluate(fractio<TypeNum, TypeDenum> const& fr) {
-        return EvalType(fr.numerator) / EvalType(fr.denumerator);
+    template<typename EvalType, typename Type>
+    EvalType fractio_cast(fractio<Type> const& fr) {
+        return EvalType(fr.numerator()) / EvalType(fr.denominator());
+    }
+
+    template<typename Type>
+    fractio<Type> inverse(fractio<Type> obj) {
+        return obj.invert();
+    }
+
+    template<typename Type>
+    bool operator==(fractio<Type> const& lhs, Type const& rhs) {
+        return lhs.numerator() == rhs
+            && (lhs.denominator() == Type(1)
+                || lhs.numerator() == Type(0));
+    }
+
+    template<typename Type>
+    bool operator==(Type const& lhs, fractio<Type> const& rhs) {
+        return rhs == lhs; 
+    }
+
+    template<typename Type>
+    bool operator!=(fractio<Type> const& lhs, Type const& rhs) {
+        return !(lhs == rhs);
+    }
+
+    template<typename Type>
+    bool operator!=(Type const& lhs, fractio<Type> const& rhs) {
+        return !(lhs == rhs);
+    }
+
+    template<typename Type>
+    std::ostream& operator<<(std::ostream& os, fractio<Type> const& frac) {
+        os << frac.numerator() << "/" << frac.denominator();
+        return os;
     }
 
 }
--- test/aid/fractio.cpp
+++ test/aid/fractio.cpp
@@ -7,8 +7,43 @@ TEST_CASE("aid::fractio") {
     SECTION("evaluation works as expected") {
         aid::fractio<int> fraction1(123);
         aid::fractio<int> fraction2(4, 2);
-        REQUIRE(aid::evaluate<double>(fraction1) == 123.0);
-        REQUIRE(aid::evaluate<double>(fraction2) == 2.0);
+        REQUIRE(aid::fractio_cast<double>(fraction1) == 123.0);
+        REQUIRE(aid::fractio_cast<double>(fraction2) == 2.0);
+    }
+
+    SECTION("implicit conversion from integral types to fractio types") {
+        aid::fractio<int> fractio1 = 123;
+        aid::fractio<unsigned> fractio2 = 123u;
+        aid::fractio<long> fractio3 = 123l;
+        aid::fractio<long long> fractio4 = 123ll;
+        REQUIRE(fractio1 == 123);
+        REQUIRE(fractio2 == 123u);
+        REQUIRE(fractio3 == 123l);
+        REQUIRE(fractio4 == 123ll);
+    }
+
+    SECTION("inversion") {
+        aid::fractio<int> fractio1(16, 14);
+        aid::fractio<int> fractio2(1, 14);
+        auto inverse1 = aid::inverse(fractio1);
+        auto inverse2 = aid::inverse(fractio2);
+        REQUIRE(inverse1.numerator() == 14);
+        REQUIRE(inverse1.denominator() == 16);
+        REQUIRE(inverse2.numerator() == 14);
+        REQUIRE(inverse2.denominator() == 1);
+    }
+
+    SECTION("multiplication") {
+        aid::fractio<int> fractio1(7, 3);
+        aid::fractio<int> fractio2(12, 14);
+        REQUIRE((fractio1 * 0) == 0);
+        REQUIRE(aid::fractio_cast<double>(fractio1 * fractio2) == 2.0);
+    }
+
+    SECTION("division") {
+        aid::fractio<int> fractio1 = 3;
+        aid::fractio<int> fractio2 = 9;
+        REQUIRE(aid::fractio_cast<double>(fractio2 / fractio1) == 3.0);
     }
 
 }
